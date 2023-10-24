4f286d473e63b5fd9a63bf11090903da06a5f82d
s22454 <s22454@pjwstk.edu.pl>
2023-10-24 17:51:10 +0200

    start of work on C# ml.net

---
+++ Birds/Birds/ImageData.cs
@@ -0,0 +1,7 @@
+﻿namespace Birds;
+
+public class ImageData
+{
+    public string ImagePath { get; set; }
+    public string Label { get; set; }
+}
---
+++ Birds/Birds/ModelInput.cs
@@ -0,0 +1,9 @@
+﻿namespace Birds;
+
+public class ModelInput
+{
+    public byte[] Image { get; set; }
+    public UInt32 LabelAsKey { get; set; }
+    public string ImagePath { get; set; }
+    public string Label { get; set; }
+}
