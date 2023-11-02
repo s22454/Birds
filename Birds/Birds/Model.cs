2061b0761488339fe11d397e35f0b31b9e371117
s22454 <s22454@pjwstk.edu.pl>
2023-11-02 17:29:11 +0100

    little code cleaning

--- Birds/Birds/Model.cs
+++ Birds/Birds/Model.cs
@@ -4,62 +4,54 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.IO;
-using Birds;
 using Microsoft.ML;
 using static Microsoft.ML.DataOperationsCatalog;
 using Microsoft.ML.Vision;
 
 public class Model
 {
-    // Paths and pre-initialized variables
-    private static string projectDirectory = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "../../../"));
-    private static string workspaceRelativePath = Path.Combine(projectDirectory, "Workspace");
-    private static string assetsRelativePath = Path.Combine(projectDirectory, "Assets");
-    
-    public static void Main(string[] args)
-    {
-        // Initialize MLContext
-        MLContext mlContext = new MLContext();
-        
-        // Load image data
-        IEnumerable<ImageData> images = LoadImagesFromDirectory(folder: assetsRelativePath, useFolderNameAsLabel: true);
-        IDataView imageData = mlContext.Data.LoadFromEnumerable(images);
+    // Model instance
+    private static Model? _modelInstance;
 
-        // Shuffle the data
-        IDataView shuffledData = mlContext.Data.ShuffleRows(imageData);
+    // Trained model and MlContext
+    private ITransformer? _trainedModel;
+    private readonly MLContext _mlContext = new MLContext();
+
+    // Data sets
+    private IDataView? _trainSet;
+    private IDataView? _validationSet;
+    private IDataView? _testSet;
+
+    private Model() {}
+    
+    public static Model GetInstance()
+    {
+        return _modelInstance ??= new Model();
+    }
+        
+    public void TrainModel(string dataPath)
+    {
+        // Load image data
+        IEnumerable<ImageData> images = LoadImagesFromDirectory(folder: dataPath, useFolderNameAsLabel: true);
+        IDataView imageData = _mlContext.Data.LoadFromEnumerable(images);
         
         // Preprocess that data 
-        var preprocessingPipeline = mlContext.Transforms.Conversion
-            .MapValueToKey(
-                inputColumnName: "Label",
-                outputColumnName: "LabelAsKey")
-            .Append(mlContext.Transforms.LoadRawImageBytes(
-                        outputColumnName: "Image",
-                        imageFolder: assetsRelativePath,
-                        inputColumnName: "ImagePath"
-                        ));
-        
-        // Use GPU
-        
-        // Apply data to preprocessingPipeline 
-        IDataView preProcessedData = preprocessingPipeline
-            .Fit(shuffledData)
-            .Transform(shuffledData);
+        IDataView preProcessedData = PreProcessData(imageData, dataPath);
         
         // Split dataset into training, validation and test sets
-        TrainTestData trainSplit = mlContext.Data.TrainTestSplit(data: preProcessedData, testFraction: .3);
-        TrainTestData validationTestSplit = mlContext.Data.TrainTestSplit(trainSplit.TestSet);
+        TrainTestData trainSplit = _mlContext.Data.TrainTestSplit(data: preProcessedData, testFraction: .3);
+        TrainTestData validationTestSplit = _mlContext.Data.TrainTestSplit(trainSplit.TestSet);
 
-        IDataView trainSet = trainSplit.TrainSet;
-        IDataView validationSet = validationTestSplit.TrainSet;
-        IDataView testSet = validationTestSplit.TestSet;
+        _trainSet = trainSplit.TrainSet;
+        _validationSet = validationTestSplit.TrainSet;
+        _testSet = validationTestSplit.TestSet;
         
         // ImageClassificationTrainer
         var classifierOptions = new ImageClassificationTrainer.Options()
         {
             FeatureColumnName = "Image",
             LabelColumnName = "LabelAsKey",
-            ValidationSet = validationSet,
+            ValidationSet = _validationSet,
             Arch = ImageClassificationTrainer.Architecture.ResnetV2101,
             MetricsCallback = (metrics) => Console.WriteLine(metrics),
             TestOnTrainSet = false,
@@ -68,20 +60,14 @@ public class Model
         };
         
         // Training pipeline 
-        var trainingPipeline = mlContext.MulticlassClassification.Trainers.ImageClassification(classifierOptions)
-            .Append(mlContext.Transforms.Conversion.MapKeyToValue("PredictedLabel"));
+        var trainingPipeline = _mlContext.MulticlassClassification.Trainers.ImageClassification(classifierOptions)
+            .Append(_mlContext.Transforms.Conversion.MapKeyToValue("PredictedLabel"));
         
         // Train the model
-        ITransformer trainedModel = trainingPipeline.Fit(trainSet);
-        
-        // Classify single image
-        ClassifySingleImage(mlContext, testSet, trainedModel);
-        
-        // Classifying multiple images
-        ClassifyImages(mlContext, testSet, trainedModel);
+        _trainedModel = trainingPipeline.Fit(_trainSet);
     }
     
-    public static IEnumerable<ImageData> LoadImagesFromDirectory(string folder, bool useFolderNameAsLabel = true)
+    private static IEnumerable<ImageData> LoadImagesFromDirectory(string folder, bool useFolderNameAsLabel = true)
     {
         // Get all files from folder 
         var files = Directory.GetFiles(folder, "*", searchOption: SearchOption.AllDirectories);
@@ -118,41 +104,147 @@ public class Model
         }
     }
 
-    public static void OutputPrediction(ModelOutput prediction)
+    public IDataView PrepareDataFromDirectory(string dataPath)
+    {
+        // Load image data
+        IEnumerable<ImageData> images = LoadImagesFromDirectory(folder: dataPath, useFolderNameAsLabel: true);
+        IDataView imageData = _mlContext.Data.LoadFromEnumerable(images);
+        
+        // Preprocess that data 
+        IDataView preProcessedData = PreProcessData(imageData, dataPath);
+        
+        // Return loaded and preprocessed images
+        return preProcessedData;
+    }
+
+    private IDataView PreProcessData(IDataView imageData, string dataPath)
+    {
+        // Shuffle the data
+        IDataView shuffledData = _mlContext.Data.ShuffleRows(imageData);
+        
+        // Preprocess that data 
+        var preprocessingPipeline = _mlContext.Transforms.Conversion
+            .MapValueToKey(
+                inputColumnName: "Label",
+                outputColumnName: "LabelAsKey")
+            .Append(_mlContext.Transforms.LoadRawImageBytes(
+                outputColumnName: "Image",
+                imageFolder: dataPath,
+                inputColumnName: "ImagePath"
+            ));
+        
+        // Apply data to preprocessingPipeline and return it
+        var preProcessedData = preprocessingPipeline
+            .Fit(shuffledData)
+            .Transform(shuffledData);
+        
+        // Return data
+        return preProcessedData;
+    }
+
+    public void TestModel()
+    {
+        // Write error if testSet wasn't set
+        if (_testSet is null)
+        {
+            Console.WriteLine("ERROR: TestSet is null!");
+            return;
+        }
+        
+        // Make predictions
+        IEnumerable<ModelOutput> predictions = ClassifyImages(_testSet);
+        
+        // Output predictions and stats
+        Console.WriteLine();
+        Console.WriteLine("-----------------------------------------------------");
+        Console.WriteLine("Testing the model");
+        Console.WriteLine("-----------------------------------------------------");
+        OutputPredictions(predictions);
+    }
+
+    public static void OutputSinglePrediction(ModelOutput prediction)
     {
         string imageName = Path.GetFileName(prediction.ImagePath);
         Console.WriteLine($"Image: {imageName} | Actual Value: {prediction.Label} | Predicted Value: {prediction.PredictedLabel}");
     }
 
-    public static void ClassifySingleImage(MLContext mlContext, IDataView data, ITransformer trainedModel)
+    public static void OutputPredictions(IEnumerable<ModelOutput> predictions)
     {
+        // Initialize variables 
+        int dataCount = 0;
+        int goodPredictions = 0;
+
+        // Iterate over predictions set
+        foreach (ModelOutput prediction in predictions)
+        {
+            dataCount++;
+
+            if (prediction.Label.Equals(prediction.PredictedLabel))
+                goodPredictions++;
+            
+            OutputSinglePrediction(prediction);
+        }
+        
+        // Write results to console 
+        Console.WriteLine("-----------------------------------------------------");
+        Console.WriteLine("Model statistics");
+        Console.WriteLine("-----------------------------------------------------");
+        Console.WriteLine("Data count: " + dataCount);
+        Console.WriteLine("Correct predictions: " + goodPredictions);
+        Console.WriteLine("Accuracy: " + goodPredictions / (float)dataCount);
+    }
+
+    public ModelOutput ClassifySingleImage(IDataView data)
+    {
+        // Return if model wasn't trained
+        if (_trainedModel is null)
+            return new ModelOutput()
+            {
+                ImagePath = "No model was trained!",
+                Label = "No model was trained!",
+                PredictedLabel = "No model was trained!"
+            };
+        
         // Create prediction engine 
         PredictionEngine<ModelInput, ModelOutput> predictionEngine =
-            mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(trainedModel);
+            _mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(_trainedModel);
 
         // Convert IDataView into IEnumerable
-        ModelInput image = mlContext.Data.CreateEnumerable<ModelInput>(data, reuseRowObject: true).First();
+        ModelInput image = _mlContext.Data.CreateEnumerable<ModelInput>(data, reuseRowObject: true).First();
         
         // Classify image
         ModelOutput prediction = predictionEngine.Predict(image);
         
-        // Output prediction 
-        Console.WriteLine("Classifying single image");
-        OutputPrediction(prediction);
+        // Return prediction 
+        return prediction;
     }
 
-    public static void ClassifyImages(MLContext mlContext, IDataView data, ITransformer trainedModel)
+    public IEnumerable<ModelOutput> ClassifyImages(IDataView data)
     {
+        // Return if model wasn't trained
+        if (_trainedModel is null)
+        {
+            ModelOutput[] error = new ModelOutput[1];
+            
+            error[0] = new ModelOutput()
+            {
+                ImagePath = "No model was trained!",
+                Label = "No model was trained!",
+                PredictedLabel = "No model was trained!"
+            };
+
+            return error;
+        }
+
+        
         // IDataView containing predictions
-        IDataView predictionData = trainedModel.Transform(data);
+        IDataView predictionData = _trainedModel.Transform(data);
         
         // Convert data into IEnumerable
         IEnumerable<ModelOutput> predictions =
-            mlContext.Data.CreateEnumerable<ModelOutput>(predictionData, reuseRowObject: true).Take(10);
+            _mlContext.Data.CreateEnumerable<ModelOutput>(predictionData, reuseRowObject: true);
         
         // Iterate and output 
-        Console.WriteLine("Classifying multiple images");
-        foreach (var prediction in predictions)
-            OutputPrediction(prediction);
+        return predictions;
     }
 }
---
+++ Birds/Birds/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.ML;
+
+namespace Birds;
+
+public class Program
+{
+    // Paths and pre-initialized variables
+    private static string projectDirectory = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "../../../"));
+    private static string workspaceRelativePath = Path.Combine(projectDirectory, "Workspace");
+    private static string assetsRelativePath = Path.Combine(projectDirectory, "Assets");
+    
+    // Manual testing 
+    private static string testingRelativePath = Path.Combine(projectDirectory, "Test");
+    
+    public static void Main(string[] args)
+    {
+        Model model = Model.GetInstance();
+        
+        // Train the model
+        model.TrainModel(assetsRelativePath);
+        
+        // Test the model
+        model.TestModel();
+        
+        // Manual testing 
+        IDataView manualTestData = model.PrepareDataFromDirectory(testingRelativePath);
+        ModelOutput predictionsResults = model.ClassifySingleImage(manualTestData);
+        Console.WriteLine();
+        Console.WriteLine("-----------------------------------------------------");
+        Console.WriteLine("Manual test");
+        Console.WriteLine("-----------------------------------------------------");
+        Model.OutputSinglePrediction(predictionsResults);
+    }
+}
